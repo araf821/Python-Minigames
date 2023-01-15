7d96e815ebc07228d68d63e14f9b5788acb6e1c5
araf821 <araf.ahmed200@gmail.com>
2023-01-15 15:46:05 -0500

    food functionality implemented

--- snakegame/food.py
+++ snakegame/food.py
@@ -1,6 +1,7 @@
 from turtle import Turtle
 import random
 
+
 class Food(Turtle):
     def __init__(self) -> None:
         super().__init__()
@@ -9,6 +10,11 @@ class Food(Turtle):
         self.shapesize(0.5)
         self.color("pink")
         self.speed("fastest")
+        self.refresh()
+
+    def refresh(self):
         x = random.randint(-360, 360)
         y = random.randint(-360, 360)
+        if y % 20 != 0:
+            y += 10
         self.goto(x, y)
--- snakegame/main.py
+++ snakegame/main.py
@@ -10,18 +10,25 @@ screen.title("Snek")
 screen.tracer(0)
 screen.listen()
 
+# Initialize snake and its food
 snake = Snake()
 food = Food()
 
+# Keyboard controls
+screen.onkey(fun=snake.move_up, key="w")
+screen.onkey(fun=snake.move_left, key="a")
+screen.onkey(fun=snake.move_down, key="s")
+screen.onkey(fun=snake.move_right, key="d")
+
 play = True
 while play == True:
-    screen.onkey(fun=snake.move_up, key="w")
-    screen.onkey(fun=snake.move_left, key="a")
-    screen.onkey(fun=snake.move_down, key="s")
-    screen.onkey(fun=snake.move_right, key="d")
     screen.update()
     sleep(0.1)
     snake.move()
+
+    # Detecting collisions
+    if snake.head.distance(food) < 20:
+        food.refresh()
         
 
 screen.exitonclick()
