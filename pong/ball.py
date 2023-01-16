0557d55f5e19cb6e94a6914b544e4d75c26acef0
araf821 <araf.ahmed200@gmail.com>
2023-01-16 09:17:18 -0500

    ball collision detection implemented

--- pong/ball.py
+++ pong/ball.py
@@ -1,6 +1,9 @@
 from turtle import Turtle
+import random
 
 class Ball(Turtle):
+    random_y = 0
+    random_x = 0
     def __init__(self) -> None:
         super().__init__()
         self.create_ball()
@@ -8,5 +11,11 @@ class Ball(Turtle):
     def create_ball(self):
         self.shape("circle")
         self.pu()
+        # self.speed(100)
         self.color("white")
-        
+    
+    def move(self):
+        self.goto(self.xcor() + self.random_x,self.ycor() + self.random_y)
+
+    def reset_position(self):
+        self.goto(0, 0)
--- pong/main.py
+++ pong/main.py
@@ -3,6 +3,7 @@ from time import sleep
 from score import Score
 from ball import Ball
 from paddle import Paddle, CompPaddle
+import random
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -16,23 +17,30 @@ player_paddle = Paddle()
 computer_paddle = CompPaddle()
 ball = Ball()
 
+# Assiging random movements for the ball
+coordinates = [-2, 2]
+ball.random_y = random.choice(coordinates)
+ball.random_x = random.choice(coordinates)
+
 screen.onkey(key="w", fun=player_paddle.up)
 screen.onkey(key="s", fun=player_paddle.down)
 
 screen.update()
 screen.tracer(1)
 
-
-
-
-
-
-
-
-
-
-
-
+play = True
+while play:
+    ball.move()
+    if ball.xcor() > 380 or ball.xcor() < -380:
+        ball.random_x *= -1
+    elif ball.ycor() > 280 or ball.ycor() < -280:
+        ball.random_y *= -1
+    elif ball.distance(player_paddle) < 50 and ball.xcor() < -330:
+        ball.random_x *= -1
+        print("collided")
+    elif ball.distance(computer_paddle) < 50 and ball.xcor() > 340:
+        ball.random_x *= -1
+        print("collided")
 
 
 screen.exitonclick()
